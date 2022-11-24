67cc51cbc284ca67bd6ceb09a5ac5539ed7bd064
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-24 21:42:44 +0530

    GO Programs

--- app.go
+++ app.go
@@ -3,5 +3,22 @@ package main
 import ("fmt")
 
 func main() { 
+  //Printing Hello World
   fmt.Println("Hello World!")
+
+  //Variables:
+  //Declaration of variable-If we have declared some variable,then its necessary to use it too.
+  var a int
+  fmt.Println(a)
+  //Initialization of Variable
+  a=55
+  fmt.Println(a)
+  //Declaration with Initialization
+  var b int=500
+  fmt.Println(b)
+  //More verbose method 
+  var c=100
+  fmt.Println(c)
+
+  
 }
