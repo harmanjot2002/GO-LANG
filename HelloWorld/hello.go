341b8511aa8d1e6f0c952f74ec56001bd0c62e8d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-24 23:01:01 +0530

    Hello World

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -5,19 +5,4 @@ import ("fmt")
 func main() { 
   //Printing Hello World
   fmt.Println("Hello World!")
-
- 
- //Constants
- //The value of a constant must be assigned when you declare it.
- const PI = 3.14
- fmt.Println(PI) //3.14
-//  Typed Constants
-// Typed constants are declared with a defined type:
-const A int=1
-fmt.Println(A) //1
-// Untyped Constants
-// Untyped constants are declared without a type
-
-
-
 }
