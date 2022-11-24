25a12eb632937cc20b9165610968178898ca612d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-24 23:00:20 +0530

    Constants

---
+++ Constantss/const.go
@@ -0,0 +1,19 @@
+package main
+
+import ("fmt")
+
+func main() { 
+ //Constants
+ //The value of a constant must be assigned when you declare it.
+ const PI = 3.14
+ fmt.Println(PI) //3.14
+
+//  Typed Constants
+// Typed constants are declared with a defined type:
+
+const A int=1
+fmt.Println(A) //1
+// Untyped Constants
+// Untyped constants are declared without a type
+
+}
