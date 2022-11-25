cce816d8e0cf219b8b4249a4e64b3d9c8b4fae7a
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-25 14:04:56 +0530

    Constants

--- Constants/const.go
+++ Constants/const.go
@@ -13,6 +13,14 @@ const (
 	a=iota
 	b=iota
 )
+
+const (
+	x1=iota
+	_
+	y1
+	z1
+)
+
 func main() { 
 	//The value of a constant must be assigned when you declare it.
 	const PI = 3.14
@@ -33,4 +41,8 @@ func main() {
 	//Value of iota applies to its own group only,in next group it will again start from 0.
 	fmt.Println(a)
 	fmt.Println(b)
+	//Here underscore(_) takes place of 1.
+	fmt.Println(x1)
+	fmt.Println(y1)
+	fmt.Println(z1)
 }
