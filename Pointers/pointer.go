83b079b64149cbfacbfb14eb67a9bee519c3e086
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-28 08:28:23 +0530

    Pointers

--- Pointers/pointer.go
+++ Pointers/pointer.go
@@ -2,10 +2,19 @@ package main
 
 import ("fmt")
 
+type Employee struct{
+	EmpId string
+}
+
 func main(){
 	var x int=90
 	var b *int=&x
 	fmt.Println(x)
 	fmt.Println(b)
 	fmt.Println(*b) //Dereferencing
+
+	var emp *Employee
+	fmt.Println(emp)
+	emp=new(Employee)
+	fmt.Println(emp)
 }
